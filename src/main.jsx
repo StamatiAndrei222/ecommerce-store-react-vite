516f102ca0e1c5adc154f6a8f482e4f5b9af07ae
Stamati Andrei <andreinustamati@gmail.com>
2025-10-18 21:45:26 +0300

    Create multiple pages using react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
+// in this file we set up the react app and render it to the dom(we render our App component on the page)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
