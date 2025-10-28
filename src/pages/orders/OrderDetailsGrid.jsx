cb2be31ece42cbf7de3d90879cdbfdba1f7b00bb
Stamati Andrei <andreinustamati@gmail.com>
2025-10-28 15:52:39 +0200

    Add to cart in orders

--- src/pages/orders/OrderDetailsGrid.jsx
+++ src/pages/orders/OrderDetailsGrid.jsx
@@ -1,12 +1,21 @@
 import dayjs from "dayjs";
+import axios from "axios";
 import BuyAgainIcon from '../../assets/images/icons/buy-again.png';
 import { Fragment } from "react";
 import { Link } from 'react-router';
 
-export function OrderDetailsGrid({ order }) {
+export function OrderDetailsGrid({ order, loadCart }) {
+
   return (
     <div className="order-details-grid">
       {order.products.map((orderProduct) => {
+          const addToCart = async () => {
+            await axios.post('/api/cart-items', {
+              productId: orderProduct.product.id,
+              quantity: 1
+            });
+            await loadCart();
+          };
         return (
           <Fragment key={orderProduct.product.id}>
             <div className="product-image-container">
@@ -23,9 +32,11 @@ export function OrderDetailsGrid({ order }) {
               <div className="product-quantity">
                 Quantity: {orderProduct.quantity}
               </div>
-              <button className="buy-again-button button-primary">
+              <button className="buy-again-button button-primary" onClick={addToCart}>
                 <img className="buy-again-icon" src={BuyAgainIcon} />
-                <span className="buy-again-message">Add to Cart</span>
+                <span className="buy-again-message">
+                  Add to Cart
+                </span>
               </button>
             </div>
 
