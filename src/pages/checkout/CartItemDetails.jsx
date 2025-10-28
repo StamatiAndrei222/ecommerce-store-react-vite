a1ea25f696ed17728f29419f0b4660d702479e67
Stamati Andrei <andreinustamati@gmail.com>
2025-10-28 18:16:16 +0200

    In the checkout page make the update quantity feature work

--- src/pages/checkout/CartItemDetails.jsx
+++ src/pages/checkout/CartItemDetails.jsx
@@ -1,7 +1,23 @@
 import { formatMoney } from '../../utils/money.js';
 import axios from 'axios';
+import { useState } from 'react';
 
 export function CartItemDetails({ cartItem, loadCart }) {
+
+  const [isUpdatingQuantity, setIsUpdatingQuantity] = useState(false);
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+
+  const inputQuantity = async () => {
+    if(isUpdatingQuantity){
+      await axios.put(`/api/cart-items/${cartItem.productId}`, {
+        quantity: Number(quantity),
+      });
+      await loadCart();
+      setIsUpdatingQuantity(false);
+    }else{
+      setIsUpdatingQuantity(true);
+    }
+  }
 
   const deleteCartItem = async () => {
     await axios.delete(`/api/cart-items/${cartItem.productId}`);
@@ -22,9 +38,23 @@ export function CartItemDetails({ cartItem, loadCart }) {
         </div>
         <div className="product-quantity">
           <span>
-            Quantity: <span className="quantity-label">{cartItem.quantity}</span>
+            Quantity: {isUpdatingQuantity
+              ? <input className="input-quantity" type="text" value={quantity} onChange={(event) => {
+                  setQuantity(event.target.value);
+              }} onKeyDown={(event) => {
+                if(event.key === 'Enter'){
+                  inputQuantity();
+                }else if(event.key === 'Escape'){
+                  setIsUpdatingQuantity(false);
+                  setQuantity(cartItem.quantity);
+                }
+              }}/>
+              : <span className="quantity-label">{cartItem.quantity}</span>
+            }
+            
           </span>
-          <span className="update-quantity-link link-primary">
+          <span className="update-quantity-link link-primary"
+            onClick={inputQuantity}>
             Update
           </span>
           <span className="delete-quantity-link link-primary"
--- src/pages/checkout/DeliveryOptions.jsx
+++ src/pages/checkout/DeliveryOptions.jsx
@@ -17,7 +17,7 @@ export function DeliveryOptions({ cartItem, deliveryOptions, loadCart }) {
         }
 
         const updateDeliveryOption = async () => {
-          await axios.put(`/api/cart-items/${cartItem.productId} `, {
+          await axios.put(`/api/cart-items/${cartItem.productId}`, {
             deliveryOptionId: deliveryOption.id,
          });
          await loadCart();
