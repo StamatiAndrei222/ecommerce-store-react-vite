083cb0b2e677cd5728eb8abcaba17809b383ff4b
Stamati Andrei <andreinustamati@gmail.com>
2025-10-29 17:23:58 +0200

    Make the search bar at the top at the top of the home page work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { NavLink } from 'react-router';
+import { useState } from 'react';
+import { useNavigate, useSearchParams } from 'react-router';
 import CartIcon from '../assets/images/icons/cart-icon.png';
 import SearchIcon from '../assets/images/icons/search-icon.png';
 import LogoWhite from '../assets/images/logo-white.png';
@@ -6,12 +8,26 @@ import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
 import './header.css';
 
 export function Header({ cart }) {
+  const [searchParams] = useSearchParams();
+
+  const searchText = searchParams.get('search');
+
+  const [search, setSearch] = useState(searchText || '');
+
+  const navigate = useNavigate();
+
   let totalQuantity = 0;
 
   cart.forEach((cartItem) => {
     totalQuantity += cartItem.quantity;
   });
 
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+  const searchProducts = () => {
+    navigate(`/?search=${search}`);
+  };
 
   return (
     <div className="header">
@@ -25,9 +41,9 @@ export function Header({ cart }) {
       </div>
 
       <div className="middle-section">
-        <input className="search-bar" type="text" placeholder="Search" />
+        <input className="search-bar" type="text" placeholder="Search" value={search} onChange={updateSearchInput}/>
 
-        <button className="search-button">
+        <button className="search-button" onClick={searchProducts}>
           <img className="search-icon" src={SearchIcon} />
         </button>
       </div>
